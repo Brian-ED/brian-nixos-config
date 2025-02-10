689b252b6c5d423598a77b38f1acece2bc5089e9
Brian E <72316548+Brian-ED@users.noreply.github.com>
2025-02-10 12:41:35 +0100

    Added working locale settings

--- configuration.nix
+++ configuration.nix
@@ -29,19 +29,19 @@
   time.timeZone = "Europe/Copenhagen";
 
   # Select internationalisation properties.
-  #i18n.defaultLocale = "en_DK.UTF-8";
+  i18n.defaultLocale = "en_DK.UTF-8";
 
-  #i18n.extraLocaleSettings = {
-  #  LC_ADDRESS = "da_DK.UTF-8";
-  #  LC_IDENTIFICATION = "da_DK.UTF-8";
-  #  LC_MEASUREMENT = "da_DK.UTF-8";
-  #  LC_MONETARY = "da_DK.UTF-8";
-  #  LC_NAME = "da_DK.UTF-8";
-  #  LC_NUMERIC = "da_DK.UTF-8";
-  #  LC_PAPER = "da_DK.UTF-8";
-  #  LC_TELEPHONE = "da_DK.UTF-8";
-  #  LC_TIME = "da_DK.UTF-8";
-  #};
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS        = "da_DK.UTF-8";
+    LC_IDENTIFICATION = "da_DK.UTF-8";
+    LC_MEASUREMENT    = "da_DK.UTF-8";
+    LC_MONETARY       = "da_DK.UTF-8";
+    LC_NAME           = "da_DK.UTF-8";
+    LC_NUMERIC        = "da_DK.UTF-8";
+    LC_PAPER          = "da_DK.UTF-8";
+    LC_TELEPHONE      = "da_DK.UTF-8";
+    LC_TIME           = "da_DK.UTF-8";
+  };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
