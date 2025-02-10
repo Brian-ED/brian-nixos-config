75c55a90bdb859a405a85e78e3d99e29b85bff05
Brian E <72316548+Brian-ED@users.noreply.github.com>
2025-02-10 12:29:03 +0100

    Added sops-nix

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
   inputs.home-manager.url = github:nix-community/home-manager;
+  inputs.sops-nix.url = "github:Mic92/sops-nix";
 
   outputs = { self, nixpkgs, ... }@attrs: {
 
