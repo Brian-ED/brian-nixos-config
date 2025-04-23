bff504d3c0c936e4a813f92c1f17d0c2ab4a7eb0
Brian-ED <brianellingsgaard9@gmail.com>
2025-04-23 10:57:12 +0200

    Added python-imports & firefoxdev also improved vscode-extension syntax a ton

--- home.nix
+++ home.nix
@@ -14,14 +14,12 @@
 
   # Install Nix packages
   home.packages = with pkgs; [
-    vim       # Code editor for when I get bored of vscodium, or when sudo-privilages is required. TODO: replace with nix-nvim
     sops      # Encrypted secrets viewer and editor. TODO: Is it supposed to replace KeePassXC?
     keepassxc # Password manager. TODO: Needs to be configured
     cbqn   # BQN programming language
     baobab # Drive space tree-like view
     restic # Backup the borgBackup folder at drive/backup-brian-Lenovo-Yoga-C940-14IIL-LinuxMintCinamon
     obsidian # Unfree package. Can only use for non-profit.
-    python314 # python 3.14
     xorg.xkbcomp # Temporary for messing with my keyboard settings
     nodejs_23 # Javascript interpreter
     pgadmin4 # Postgresql for database connection
@@ -42,6 +40,20 @@
     rustc cargo # Rust stuff
     audacious # For playing music
     nil # Nix langauge server
+    firefox-devedition
+    firefox
+
+    # Python with scientific libraries
+    (
+      python3.withPackages (p: with p;[
+        numpy matplotlib sympy pandas # Me want very much. Used often.
+        jupyter ipython ipykernel # Jupiter and dependencies
+        mpmath # Arbitrary precision math
+        scipy # Lots of mathy functions like eigenvalues and curve fitting
+        scikit-learn # AI
+      ])
+    )
+
     (vscode-with-extensions.override {
       vscode = vscodium;
       vscodeExtensions = with vscode-extensions; [
@@ -51,7 +63,6 @@
         esbenp.prettier-vscode
         formulahendry.code-runner
         vscode-extensions."13xforever".language-x86-64-assembly
-        ms-vscode-remote.remote-wsl
         bradlc.vscode-tailwindcss
         rust-lang.rust-analyzer
         ms-vscode-remote.remote-ssh-edit
@@ -64,130 +75,30 @@
         github.copilot-chat
         hediet.vscode-drawio
         ms-vscode.cpptools
-# HTML to CSS autocompletion
-
-      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-        {
-          name = "bqn";
-          publisher = "mk12";
-          version = "0.1.7";
-          sha256 = "18arx9nrqwlpx7b5qq9w83p4cbicz6d40x3447g300gqapfhlb3j";
-        }
-        {
-          name = "newline";
-          publisher = "chang196700";
-          version = "0.0.4";
-          sha256 = "0xijg1nqlrlwkl4ls21hzikr30iz8fd98ynpbmhhdxrkm3iccqa1";
-        }
-        {
-          name = "tws"; # Trailing white space
-          publisher = "jkiviluoto";
-          version = "1.0.1";
-          sha256 = "0aj58iasgnmd2zb7zxz587k9mfmykjwrb8h7hfvpkmh76s9bj4y5";
-        }
-        {
-          name = "toggle-zen-mode";
-          publisher = "fudd";
-          version = "1.1.2";
-          sha256 = "0whmbpnin1r1qnq45fpz7ayp51d4lilvbnv7llqd6jplx5b4n3ds";
-        }
-        {
-          name = "todo-tree";
-          publisher = "Gruntfuggly";
-          version = "0.0.226";
-          sha256 = "0yrc9qbdk7zznd823bqs1g6n2i5xrda0f9a7349kknj9wp1mqgqn";
-        }
-        {
-          name = "iceworks-time-master";
-          publisher = "iceworks-team";
-          version = "1.0.4";
-          sha256 = "05k7icssa7llbp4a44kny0556hvimmdh6fm394y5rh86bxqq0iq3";
-        }
-        {
-          name = "suteppu";
-          publisher = "Itsakaseru";
-          version = "1.0.1";
-          sha256 = "1z0zkznwwm0z1vyq2wsw9rf1kg8pfpb3rl7glx0zp3aq8sxvnfsf";
-        }
-        {
-          name = "vscode-sort";
-          publisher = "henriiik";
-          version = "0.2.5";
-          sha256 = "0sam2qfa596dcbabx3alrwsgm56a8wzb65dp45yv172kcaam5yd6";
-        }
-        {
-          name = "slint";
-          publisher = "Slint";
-          version = "1.10.0";
-          sha256 = "1c6bailn562zsh7wzx8jjj0y8mdzxj3sqgym5x5liw93b11kv3i3";
-        }
-        {
-          name = "remote-explorer";
-          publisher = "ms-vscode";
-          version = "0.5.2025021709";
-          sha256 = "02lnijwl92lq3rhv4f0kbp1bjfniipigih1myl5xmrwsp85n88xl";
-        }
-        {
-          name = "ols";
-          publisher = "DanielGavin";
-          version = "0.1.34";
-          sha256 = "0rl6mjkabgbwc0vnm96ax1jhjh5rrky0i1w40fhs1zqyfd83mrsx";
-        }
-        {
-          name = "vscode-lowercase";
-          publisher = "ruiquelhas";
-          version = "1.0.1";
-          sha256 = "03kwbnc25rfzsr7lzgkycwxnifv4nx04rfcvmfcqqhacx74g14gs";
-        }
-        {
-          name = "vsliveshare"; # Live Share
-          publisher = "MS-vsliveshare";
-          version = "1.0.5948";
-          sha256 = "0rhwjar2c6bih1c5w4w8gdgpc6f18669gzycag5w9s35bv6bvsr8";
-        }
-        {
-          name = "inline-html-indent";
-          publisher = "vulkd";
-          version = "0.0.1";
-          sha256 = "0mh7kpis821088g5qmzay76zrgvgbikl9v2jdjs3mdfkbh2rfl6s";
-        }
-        {
-          name = "vuerd-vscode"; # ERD editor
-          publisher = "dineug";
-          version = "2.0.5";
-          sha256 = "1agcayiz8p7n05x6wm817gdj3fwmxkdxbsf5alx4jbp1msi6qwwh";
-        }
-        {
-          name = "chatgpt-copilot";
-          publisher = "feiskyer";
-          version = "4.8.4";
-          sha256 = "0766vq07gjxgh4xpflzmrcx55i6b9w4hk5zg8yirvgfjscv5gvxv";
-        }
-        {
-          name = "vscode-apl-language-client";
-          publisher = "OptimaSystems";
-          version = "0.0.9";
-          sha256 = "050nn7f6gfzskq1yavqdw77rgl1lxs3p8dqkzrmmliqh5kqh2gr8";
-        }
-        {
-          name = "vscode-apl-language";
-          publisher = "OptimaSystems";
-          version = "0.0.7";
-          sha256 = "003n637vskbi4wypm8qwdy4fa9skp19w6kli1bgc162gzcbswhia";
-        }
-        {
-          name = "vscode-autohotkey-plus-plus";
-          publisher = "mark-wiemer";
-          version = "6.7.0";
-          sha256 = "10sf0qf0sqc5ifjf9vg2fyh7akz7swrilz6aifvyswzglglmca19";
-        }
-        {
-          name = "i3";
-          publisher = "dcasella";
-          version = "1.0.0";
-          sha256 = "0z7qj6bwch1cxr6pab2i3yqk5id8k14mjlvl6i9f0cmdsxqkmci5";
-        }
-      ];
+      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace (map (x:
+        let I=builtins.elemAt; in
+        { name=I x 0;                   publisher=I x 1; version=I x 2;   sha256=I x 3; } ) [
+        [ "bqn"                         "mk12"           "0.1.7"          "18arx9nrqwlpx7b5qq9w83p4cbicz6d40x3447g300gqapfhlb3j" ]
+        [ "newline"                     "chang196700"    "0.0.4"          "0xijg1nqlrlwkl4ls21hzikr30iz8fd98ynpbmhhdxrkm3iccqa1" ]
+        [ "tws"                         "jkiviluoto"     "1.0.1"          "0aj58iasgnmd2zb7zxz587k9mfmykjwrb8h7hfvpkmh76s9bj4y5" ] # Trailing white space
+        [ "toggle-zen-mode"             "fudd"           "1.1.2"          "0whmbpnin1r1qnq45fpz7ayp51d4lilvbnv7llqd6jplx5b4n3ds" ]
+        [ "todo-tree"                   "Gruntfuggly"    "0.0.226"        "0yrc9qbdk7zznd823bqs1g6n2i5xrda0f9a7349kknj9wp1mqgqn" ]
+        [ "iceworks-time-master"        "iceworks-team"  "1.0.4"          "05k7icssa7llbp4a44kny0556hvimmdh6fm394y5rh86bxqq0iq3" ]
+        [ "suteppu"                     "Itsakaseru"     "1.0.1"          "1z0zkznwwm0z1vyq2wsw9rf1kg8pfpb3rl7glx0zp3aq8sxvnfsf" ]
+        [ "vscode-sort"                 "henriiik"       "0.2.5"          "0sam2qfa596dcbabx3alrwsgm56a8wzb65dp45yv172kcaam5yd6" ]
+        [ "slint"                       "Slint"          "1.10.0"         "1c6bailn562zsh7wzx8jjj0y8mdzxj3sqgym5x5liw93b11kv3i3" ]
+        [ "remote-explorer"             "ms-vscode"      "0.5.2025021709" "02lnijwl92lq3rhv4f0kbp1bjfniipigih1myl5xmrwsp85n88xl" ]
+        [ "ols"                         "DanielGavin"    "0.1.34"         "0rl6mjkabgbwc0vnm96ax1jhjh5rrky0i1w40fhs1zqyfd83mrsx" ]
+        [ "vscode-lowercase"            "ruiquelhas"     "1.0.1"          "03kwbnc25rfzsr7lzgkycwxnifv4nx04rfcvmfcqqhacx74g14gs" ]
+        [ "vsliveshare"                 "MS-vsliveshare" "1.0.5948"       "0rhwjar2c6bih1c5w4w8gdgpc6f18669gzycag5w9s35bv6bvsr8" ] # Live Share
+        [ "inline-html-indent"          "vulkd"          "0.0.1"          "0mh7kpis821088g5qmzay76zrgvgbikl9v2jdjs3mdfkbh2rfl6s" ]
+        [ "vuerd-vscode"                "dineug"         "2.0.5"          "1agcayiz8p7n05x6wm817gdj3fwmxkdxbsf5alx4jbp1msi6qwwh" ] # ERD editor
+        [ "chatgpt-copilot"             "feiskyer"       "4.8.4"          "0766vq07gjxgh4xpflzmrcx55i6b9w4hk5zg8yirvgfjscv5gvxv" ]
+        [ "vscode-apl-language-client"  "OptimaSystems"  "0.0.9"          "050nn7f6gfzskq1yavqdw77rgl1lxs3p8dqkzrmmliqh5kqh2gr8" ]
+        [ "vscode-apl-language"         "OptimaSystems"  "0.0.7"          "003n637vskbi4wypm8qwdy4fa9skp19w6kli1bgc162gzcbswhia" ]
+        [ "vscode-autohotkey-plus-plus" "mark-wiemer"    "6.7.0"          "10sf0qf0sqc5ifjf9vg2fyh7akz7swrilz6aifvyswzglglmca19" ]
+        [ "i3"                          "dcasella"       "1.0.0"          "0z7qj6bwch1cxr6pab2i3yqk5id8k14mjlvl6i9f0cmdsxqkmci5" ]
+      ]);
     })
 
     # Shell scripts
