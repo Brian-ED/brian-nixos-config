44a3f1a49acd416248b5cd26ed10dcc4dac82679
Brian-ED <brianellingsgaard9@gmail.com>
2025-05-27 20:30:04 +0200

    I don't like having large coredumps stored

--- configuration.nix
+++ configuration.nix
@@ -168,6 +168,9 @@
     };
   };
 
+  # Cleanup coredumps
+  systemd.coredump.extraConfig = "MaxUse=250m";
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
