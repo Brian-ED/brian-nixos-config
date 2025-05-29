b169c8f0fd69b3cc750d96d08209db8a382601d6
Brian-ED <brianellingsgaard9@gmail.com>
2025-05-29 23:04:42 +0200

    fixed true branch of if statement It only errors if it's triggered. It wasn't.

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,7 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-{ config, pkgs, lib, brian-i3-config, inputs, ... }:
+{ config, pkgs, lib, inputs, ... }:
 {
   nix = {
     channel.enable = false;
@@ -80,7 +80,7 @@
 
 
       windowManager.i3.enable = true;
-      windowManager.i3.configFile = if false then "${brian-i3-config}/config" else "/home/brian/brian-i3-config/config";
+      windowManager.i3.configFile = if false then "${inputs.brian-i3-config}/config" else "/home/brian/brian-i3-config/config";
       windowManager.i3.extraPackages = [];
     };
 
