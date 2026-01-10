0e9517808cc5a806bdbc325e061b6ddcd427aa6f
Brian-ED <brianellingsgaard9@gmail.com>
2026-01-10 17:55:56 +0100

    added forgotten-to-push cachyos options

--- modules/cachyos-kernel.nix
+++ modules/cachyos-kernel.nix
@@ -2,6 +2,10 @@
 {
   nixpkgs.overlays = [ inputs.nix-cachyos-kernel.overlays.pinned ]; # use the exact kernel versions as defined in this repo. Guarantees you have binary cache.
   boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
+  boot.zfs.package = pkgs.cachyosKernels.linuxPackages-cachyos-latest.zfs_cachyos;
+  boot.supportedFilesystems = [ "zfs" ];
+
+  networking.hostId = "7b5827db"; # Used by ZFS, which cachyOS kernel uses
 
   # Binary cache
   nix.settings.substituters = [ "https://attic.xuyh0120.win/lantian" ];
